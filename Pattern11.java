d450d94c7e5c603f466b07fcdf85b0f680ff8d04
Mangesh <mangeshj1510@gmail.com>
2025-06-29 01:45:16 +0530

    Add files via upload

---
+++ Pattern11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Pattern11 {
+    public static void main(String[] args) {
+        System.out.println("Enter Rows:");
+        Scanner sc=new Scanner(System.in);
+        int rows=sc.nextInt();
+
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            for(int j=i-1;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
