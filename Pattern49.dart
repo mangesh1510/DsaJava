24b450671b359c16ec00ed0a003157fe4a37ccae
Mangesh <mangeshj1510@gmail.com>
2025-07-07 22:22:30 +0530

    Pattern49.dart

---
+++ Pattern49.dart
@@ -0,0 +1,27 @@
+import "dart:core";
+import 'dart:io';
+
+void main() {
+  print("enter rows");
+  int rows = int.parse(stdin.readLineSync()!);
+
+  for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= rows - i; j++) {
+      stdout.write(" ");
+    }
+    for (int j = 1; j <= i; j++) {
+      stdout.write("* ");
+    }
+    print("");
+  }
+
+  for (int i = rows - 1; i >= 1; i--) {
+    for (int j = 1; j <= rows - i; j++) {
+      stdout.write(" ");
+    }
+    for (int j = i; j >= 1; j--) {
+      stdout.write("* ");
+    }
+    print("");
+  }
+}
